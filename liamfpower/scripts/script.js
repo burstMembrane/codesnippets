a66e03fb2d72883218c9c0a9f5168b6b99e0499e
Liam <liamfpower@gmail.com>
2018-02-13 13:14:39 +1100

    significant structural changes
    added responsive design
    added news and works page

--- liamfpower/scripts/script.js
+++ liamfpower/scripts/script.js
@@ -1,48 +1,45 @@
-var canvas;
-var pixelsclone;
-var canvasx = innerWidth;
-var canvasy = innerHeight * 2 ;
-p5.disableFriendlyErrors = true;
-
-function setup() {
-frameRate(15);
-canvas = createCanvas(screen.width,  screen.height);
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
 
 
-    canvas.position(0,0,);
-    canvas.style('z-index', '-1');
-      canvas.style('background-repeat', 'repeat-y');
-    pixelDensity(1);
+canvas.width = canvas.height = 1000;
+
+function noise(ctx) {
+
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        len = buffer32.length,
+        i = 0;
+
+    for(; i < len;i++)
+        if (Math.random() < 0.9) buffer32[i] = 0xff000000;
+
+    ctx.putImageData(idata, 0, 0);
 
 }
 
-function draw() {
-    loadPixels();
+var toggle = true;
 
 
-    for(var x = 0; x < width; x++) {
-         for(var y = 0; y < height; y++)
-       {
-           var index = (x + y * width) * 4;
-           var r = random(150);
-           var r = int(r);
-            pixels[index+0] = r;
-            pixels[index+1] = r;
-            pixels[index+2] = r;
-            pixels[index+3] = 255;
+function resize() {
+    canvas.style.width = window.innerWidth + 'px';
+    canvas.style.height = window.innerHeight + 'px';
+}
+resize();
+window.onresize = resize;
 
-       }
-      //  noLoop();
+(function loop() {
 
-
+    toggle = !toggle;
+    if (toggle) {
+        requestAnimationFrame(loop);
+        return;
     }
- updatePixels();
-}
 
+    noise(ctx);
 
+    requestAnimationFrame(loop);
 
-function windowResized() {
-  resizeCanvas(window.width, window.height);
-}
-
-
+})();
